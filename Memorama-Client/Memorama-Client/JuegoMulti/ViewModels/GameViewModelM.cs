5cd2b3b22ad48506cc796b8f89194cc4fc8b008d
JONATHAN DE JESUS MORENO MARTINEZ <jhoni65475@gmail.com>
2021-01-07 15:51:49 -0600

    Agregando la base para el multijugador

---
+++ Memorama-Client/Memorama-Client/JuegoMulti/ViewModels/GameViewModelM.cs
@@ -0,0 +1,106 @@
+﻿using Memorama_Client.JuegoBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memorama_Client.JuegoMulti.ViewModels
+{
+
+    public enum SlideCategoriesM
+    {
+        Cartas,
+        Animals,
+        Cars,
+        Foods
+    }
+
+
+    public class GameViewModelM : ObservableObjectM
+    {
+        //Collection of slides we are playing with
+        public SlideCollectionViewModelM Slides { get; private set; }
+        //Game information scores, attempts etc
+        public GameInfoViewModelM GameInfo { get; private set; }
+        //Game timer for elapsed time
+        public TimerViewModelM Timer { get; private set; }
+        //Category we are playing in
+        public SlideCategoriesM Category { get; private set; }
+
+        public GameViewModelM(SlideCategoriesM category)
+        {
+            Category = category;
+            SetupGame(category);
+        }
+
+        //Initialize game essentials
+        private void SetupGame(SlideCategoriesM category)
+        {
+
+            Slides = new SlideCollectionViewModelM();
+            Timer = new TimerViewModelM(new TimeSpan(0, 0, 1));
+            GameInfo = new GameInfoViewModelM();
+
+            //Set attempts to the maximum allowed
+            GameInfo.ClearInfo();
+
+            //Create slides from image folder then display to be memorized
+            Slides.CreateSlides("Assets/" + category.ToString());
+            Slides.Memorize();
+
+            //Game has started, begin count.
+            Timer.Start();
+
+            //Slides have been updated
+            OnPropertyChanged("Slides");
+            OnPropertyChanged("Timer");
+            OnPropertyChanged("GameInfo");
+        }
+
+        //Slide has been clicked
+        public void ClickedSlide(object slide)
+        {
+            if (Slides.canSelect)
+            {
+                var selected = slide as PictureViewModelM;
+                Slides.SelectSlide(selected);
+            }
+
+            if (!Slides.areSlidesActive)
+            {
+                if (Slides.CheckIfMatched())
+                    GameInfo.Award(); //Correct match
+                else
+                    GameInfo.Penalize();//Incorrect match
+            }
+
+            GameStatus();
+        }
+
+        //Status of the current game
+        private void GameStatus()
+        {
+            if (GameInfo.MatchAttempts < 0)
+            {
+                GameInfo.GameStatus(false);
+                Slides.RevealUnmatched();
+                Timer.Stop();
+            }
+
+            if (Slides.AllSlidesMatched)
+            {
+                GameInfo.GameStatus(true);
+                Timer.Stop();
+            }
+        }
+
+        //Restart game
+        public void Restart()
+        {
+            ControladorDeAudio.PlayIncorrecto();
+            SetupGame(Category);
+        }
+
+    }
+}
---
+++ Memorama-Client/Memorama-Client/JuegoMulti/ViewModels/StartMenuViewModelM.cs
@@ -0,0 +1,27 @@
+﻿using Memorama_Client.JuegoBase;
+using Memorama_Client.JuegoBase.Vistas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memorama_Client.JuegoMulti.ViewModels
+{
+   public  class StartMenuViewModelM
+    {
+        private Juego _mainWindow;
+        public StartMenuViewModelM(Juego main)
+        {
+            _mainWindow = main;
+            ControladorDeAudio.PlayBackgroundMusic();
+        }
+
+        public void StartNewGame(int categoryIndex)
+        {
+            var category = (SlideCategoriesM)categoryIndex;
+            GameViewModelM newGame = new GameViewModelM(category);
+            _mainWindow.DataContext = newGame;
+        }
+    }
+}
