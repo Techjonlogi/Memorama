7934003e08e61c435586e14c0ae837869211fb39
JONATHAN DE JESUS MORENO MARTINEZ <jhoni65475@gmail.com>
2021-01-09 20:25:19 -0600

    agregando proyecto de pruebas

---
+++ Memorama-Client/Memorama-ClientTests/Validaciones/ValidarCamposTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Memorama_Client.Validaciones;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memorama_Client.Validaciones.Tests
+{
+    [TestClass()]
+    public class ValidarCamposTests
+    {
+        [TestMethod()]
+        public void ValidarContraseñaTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
