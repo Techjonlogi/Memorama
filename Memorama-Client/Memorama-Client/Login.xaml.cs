c6d72a30b418cb43a18ec756c5c3cead88bd0fd1
JONATHAN DE JESUS MORENO MARTINEZ <jhoni65475@gmail.com>
2021-01-11 19:25:13 -0600

    Arreglando validaciones en el login

--- Memorama-Client/Memorama-Client/Login.xaml.cs
+++ Memorama-Client/Memorama-Client/Login.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using Memorama_Client.Validaciones;
+using System.Windows;
+using static Memorama_Client.Validaciones.ValidarCampos;
 
 namespace Memorama_Client
 {
@@ -13,17 +15,71 @@ namespace Memorama_Client
             InitializeComponent();
         }
 
+
+
+        private enum ChecResults
+        {
+            Passed, Failed
+        }
+
+        private ChecResults CheckEmptyFields()
+        {
+            ChecResults check = ChecResults.Failed;
+            if (txtUsuario.Text == string.Empty || passBox.Password == string.Empty)
+            {
+                check = ChecResults.Failed;
+            }
+            else
+            {
+                check = ChecResults.Passed;
+            }
+            return check;
+        }
+
+        private ChecResults CheckFields()
+        {
+            ChecResults check = ChecResults.Failed;
+            ValidarCampos validarCampos = new ValidarCampos();
+            if (CheckEmptyFields() == ChecResults.Failed)
+            {
+                MessageBox.Show("Existen campos sin llenar");
+                check = ChecResults.Failed;
+            }
+            else if (validarCampos.ValidarContraseña(passBox.Password) == ValidarCampos.ResultadosValidacion.ContraseñaInvalida)
+            {
+                MessageBox.Show("La contraseña es muy débil \n Intenta combinar letras mayúsculas, minúsculas y números");
+            }
+            else if (validarCampos.ValidarUsuario(txtUsuario.Text) == ValidarCampos.ResultadosValidacion.UsuarioInvalido)
+            {
+                MessageBox.Show("El usuario que ingresó no es valido");
+            }
+            else 
+            {
+                check = ChecResults.Passed;
+            }
+            return check;
+        }
+
         private void btnIngresar_Click(object sender, RoutedEventArgs e)
         {
-            Memorama_Client.Properties.Settings.Default.UsuarioOnline = txtUsuario.Text;
 
 
-
-            Servicios.IniciarSesion(new ServidorMemorama.Usuario
+            if (CheckFields() == ChecResults.Passed) 
             {
-                Nickname = txtUsuario.Text,
-                Password = txtContraseña.Text
-            });
+
+
+                Memorama_Client.Properties.Settings.Default.UsuarioOnline = txtUsuario.Text;
+                Servicios.IniciarSesion(new ServidorMemorama.Usuario
+                {
+                    Nickname = txtUsuario.Text,
+                    Password = passBox.Password
+                }); ;
+                this.Close();
+
+
+            }
+
+            
 
 
 
