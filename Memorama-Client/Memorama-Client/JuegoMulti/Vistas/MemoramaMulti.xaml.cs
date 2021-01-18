bab3efe26c3c25a4cb72ff8e5d2077bdb0f0d6c4
JONATHAN DE JESUS MORENO MARTINEZ <jhoni65475@gmail.com>
2021-01-18 14:48:02 -0600

    arreglando memorama

---
+++ Memorama-Client/Memorama-Client/JuegoMulti/Vistas/MemoramaMulti.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Memorama_Client.JuegoMulti.Vistas
+{
+    /// <summary>
+    /// Lógica de interacción para MemoramaMulti.xaml
+    /// </summary>
+    public partial class MemoramaMulti : Window
+    {
+        
+        
+        public MemoramaMulti()
+        {
+            InitializeComponent();
+        }
+
+        private void Carta1_Click(object sender, RoutedEventArgs e)
+        {
+            int idCarta = 1;
+
+        }
+
+        private void Carta2_Click(object sender, RoutedEventArgs e)
+        {
+            int idCarta = 2;
+        }
+
+        private void Carta3_Click(object sender, RoutedEventArgs e)
+        {
+            int idCarta = 3;
+        }
+
+        private void Carta4_Click(object sender, RoutedEventArgs e)
+        {
+            int idCarta = 4;
+        }
+
+        private void Carta5_Click(object sender, RoutedEventArgs e)
+        {
+            int idCarta = 5;
+        }
+
+        private void Carta6_Click(object sender, RoutedEventArgs e)
+        {
+            int idCarta = 6;
+        }
+
+        private void Carta7_Click(object sender, RoutedEventArgs e)
+        {
+            int idCarta = 7;
+        }
+
+        private void Carta8_Click(object sender, RoutedEventArgs e)
+        {
+            int idCarta = 8;
+        }
+
+        private void Carta9_Click(object sender, RoutedEventArgs e)
+        {
+            int idCarta = 9;
+        }
+
+        private void Carta10_Click(object sender, RoutedEventArgs e)
+        {
+            int idCarta = 10;
+        }
+
+        private void Carta11_Click(object sender, RoutedEventArgs e)
+        {
+            int idCarta = 11;
+        }
+
+        private void Carta12_Click(object sender, RoutedEventArgs e)
+        {
+            int idCarta = 12;
+        }
+    }
+}
